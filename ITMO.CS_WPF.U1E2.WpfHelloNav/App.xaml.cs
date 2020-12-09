dd6504e71ac468fa89ef2d3cfc32a3b4b46ed9cb
AlexanderSolomin <a.solomin@ymail.com>
2020-12-09 10:43:02 +0300

    Add project files.

---
+++ ITMO.CS_WPF.U1E2.WpfHelloNav/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.CS_WPF.U1E2.WpfHelloNav
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ ITMO.CS_WPF.U2E2.WpfRoutedEvent_2_2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ITMO.CS_WPF.U2E2.WpfRoutedEvent_2_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void StackPanel_Click_1(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            double a = Double.Parse(textBox.Text);
+            switch (feSource.Name)
+            {
+                case "butAdd":
+                    a += a;
+                    break;
+                case "butMult":
+                    a *= a;
+                    break;
+            }
+            e.Handled = true;
+            textBox.Text = String.Format("{0:#.##}", a);
+        }
+    }
+}
