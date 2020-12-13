ec6b7d524f2e0caaa2c1fa537177a7ee9e72d15c
AlexanderSolomin <a.solomin@ymail.com>
2020-12-13 12:31:35 +0300

    upd

---
+++ ITMO.CS_WPF.U4E1.UserIn1/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ITMO.CS_WPF.U4E1.UserIn1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your name is " + txtFirstName.Text + " " + txtLastName.Text + "and your email address is " + txtEmail.Text);
+        }
+    }
+}
