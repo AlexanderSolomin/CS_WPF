b8c534c421e72697f66d48596721403e9056ff91
AlexanderSolomin <solo.true@yahoo.com>
2020-12-28 13:13:10 +0300

    U6E1, E2

---
+++ ITMO.CS_WPF.U6E1.UserForms/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ITMO.CS_WPF.U6E1.UserForms
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
